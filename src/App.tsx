cfa245a4e9d3fa666a3d051e00725f07d669daae
ladraniamine <ladraniful10@gmail.com>
2023-12-07 13:15:52 +0100

    ADD: table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import TableComponent from "./challenge/TableComponent";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <div>helo</div>
+      <TableComponent />
+    </>
+  );
 }
 
 export default App;
